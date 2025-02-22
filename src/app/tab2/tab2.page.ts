9dc15fbf8efe09eddfda3ce7e1a67a064613dc9c
Livingston <livingstonlvin@gmail.com>
2025-02-22 13:01:13 +0530

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFabButton,
+  IonFab,
+  IonIcon,
+  IonGrid,
+  IonCol,
+  IonImg,
+  IonRow
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { camera } from 'ionicons/icons';
+import { PhotoService } from '../services/photo.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFabButton,
+    IonFab,
+    IonIcon,
+    IonGrid,
+    IonCol,
+    IonImg,
+    IonRow,
+    NgFor
+  ],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  constructor(public photoService: PhotoService) {
+    addIcons({ camera });
+  }
+  
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
-  constructor() {}
-
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
